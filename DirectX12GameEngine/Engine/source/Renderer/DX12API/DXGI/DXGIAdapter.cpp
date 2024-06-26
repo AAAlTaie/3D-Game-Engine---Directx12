febe1a3d556529e160e2e3ec2c9cd1e94344f29b
Altaie Abobakr <aaaltaie@student.fullsail.edu>
2024-06-25 22:35:04 -0400

    added Adapter files to my repo

---
+++ DirectX12GameEngine/Engine/source/Renderer/DX12API/DXGI/DXGIAdapter.cpp
@@ -0,0 +1,29 @@
+//#include "pch/pch.h"
+#include "DXGIAdapter.h"
+
+
+namespace ENGINE 
+{
+	
+	DXGIAdapter::DXGIAdapter(IDXGIAdapter* pAdapter)  noexcept : ComPtr<IDXGIAdapter>(pAdapter)
+	{
+
+
+	}
+
+	void DXGIAdapter::FindMonitor()
+	{
+
+	}
+	
+
+
+	
+
+
+
+
+
+	
+
+}
---
+++ DirectX12GameEngine/Engine/source/Renderer/DX12API/DXGI/DXGIAdapter.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "pch/pch.h"
+
+
+namespace ENGINE
+{
+	
+	class DXGIAdapter : public Microsoft::WRL::ComPtr<IDXGIAdapter>
+	{
+	public:
+
+		DXGIAdapter() noexcept;
+
+		DXGIAdapter(IDXGIAdapter* pAdapter)  noexcept;
+
+		void FindMonitor();
+
+
+
+
+
+
+
+
+	};
+
+
+
+
+}
+
+
